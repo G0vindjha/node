6c521bec5df7c9f1669a769daf337c2944b0b86d
GOVIND JHA <jhag2491@gmail.com>
2024-08-06 18:56:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,33 @@ app.get('/',function(req,res){
 
 app.get('/create',async function(req,res){
     const user = await userModel.create({
-        name:'Govind',
-        email:'jhag2491@gamil.com',
-        username:'sam'
+        name:'Shrusti Jha',
+        email:'shrusti@gamil.com',
+        username:'shrusti'
     });
     res.send(user);
+});
+
+app.get('/update',async function(req,res){
+    const updateUser = await userModel.findOneAndUpdate({
+        username:'sam'
+    },{
+        name:'Govind Jha',
+        email:'jhagovind@gamil.com',
+    });
+    res.send(updateUser);
+});
+
+app.get('/delete',async function(req,res){
+    const deleteUser = await userModel.findOneAndDelete({
+        username:'sam'
+    });
+    res.send(deleteUser);
+});
+
+app.get('/users',async function(req,res){
+    const allUser = await userModel.find();
+    res.send(allUser);
 });
 
 app.listen(3000);
